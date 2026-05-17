8caa8de240b3fb2f123bf3c4cfdc949ae22bd247
satyamnag <154697734+satyamnag@users.noreply.github.com>
2026-05-17 16:29:10 +0000

    updated to standalone e-commerce structure from multi tenant ecommerce structure

--- src/modules/checkout/ui/components/navbar.tsx
+++ src/modules/checkout/ui/components/navbar.tsx
@@ -1,13 +1,9 @@
+// src/modules/checkout/ui/components/navbar.tsx
 import Link from "next/link";
 
-import { generateTenantURL } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
-interface Props {
-  slug: string;
-};
-
-export const Navbar = ({ slug }: Props) => {
+export const Navbar = () => {
   return (
     <nav className="h-20 border-b font-medium bg-white">
       <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
@@ -16,7 +12,7 @@ export const Navbar = ({ slug }: Props) => {
           variant="elevated"
           asChild
         >
-          <Link href={generateTenantURL(slug)}>
+          <Link href="/">
             Continue Shopping
           </Link>
         </Button>
